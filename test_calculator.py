514bbf9f35609d50f0db010912648a3dc4a4afd0
Sulman <sulmanmh@gmail.com>
2019-06-20 10:41:27 +0100

    Add tests for mult + div operations

--- test_calculator.py
+++ test_calculator.py
@@ -8,3 +8,9 @@ class TestCalculator:
 
     def test_sibtraction(self):
         assert 2 == calculator.subtract(4, 2)
+
+    def test_multiplication(self):
+        assert 8 == calculator.multiply(2, 4)
+
+    def test_division(self):
+        assert 2 == calculator.divide(10, 2)
