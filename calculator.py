55a12494e2813c7d3e20a72d80629dcafcd56a18
Sulman <sulmanmh@gmail.com>
2019-06-20 09:42:24 +0100

    Refactor flake8 feedback

--- calculator.py
+++ calculator.py
@@ -6,8 +6,8 @@ Basic math operations
 
 
 def add(first_term, second_term):
-	return first_term + second_term
+    return first_term + second_term
 
 
 def subtract(first_term, second_term):
-	return first_term - second_term	
+    return first_term - second_term
--- test_calculator.py
+++ test_calculator.py
@@ -1,11 +1,10 @@
 import calculator
 
 
-
 class TestCalculator:
 
-	def test_addition(self):
-		assert 4 ==calculator.add(2,2)
+    def test_addition(self):
+        assert 4 == calculator.add(2, 2)
 
-	def test_sibtraction(self):
-		assert 2 == calculator.subtract(4,2)		
+    def test_sibtraction(self):
+        assert 2 == calculator.subtract(4, 2)
