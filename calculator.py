fa08b61b063418cab122a2b71ccf27e026e3faf5
Sulman <sulmanmh@gmail.com>
2019-06-20 10:48:38 +0100

    Adding multiply and divide funcs

--- calculator.py
+++ calculator.py
@@ -11,3 +11,12 @@ def add(first_term, second_term):
 
 def subtract(first_term, second_term):
     return first_term - second_term
+
+
+def multiply(first_term, second_term):
+    return first_term * second_term
+
+
+def divide(first_term, second_term):
+    if first_term >= second_term:
+        return first_term / second_term
